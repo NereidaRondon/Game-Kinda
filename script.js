a9872a35694e0c98fd33287542e7c09deb32a1a2
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-07-21 14:59:26 -0400

    Trying to disable play button

--- script.js
+++ script.js
@@ -7,31 +7,66 @@
 // })
 
 
+
+//////////////////////////////////////////////////////// 
+// const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+// const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+// console.log(dropdownElementList);
+
+document.querySelector('#greet-player').setAttribute('hidden', '');
 ////////////////////////////////////////////////////////
 
-        //tabs over when you hit enter
-        function handleEnter(event) {
-            if (event.key==="Enter") {
-                const form = document.getElementById('form')
-                const index = [...form].indexOf(event.target);
-                form.elements[index + 1].focus();
-            }
-        }
-
-          
+//tabs over when you hit enter
+function handleEnter(event) {
+    if (event.key==="Enter") {
+        const form = document.getElementById('form')
+        const index = [...form].indexOf(event.target);
+        form.elements[index + 1].focus();
+    }
+}
    
 //////////////////////////////////////////////////////// 
+function disableLink() {
+  document.getElementById("startLink").disabled = true;
+}
+disableLink();
+
+function enableLink() {
+  document.getElementById("startLink").disabled = false;
+};
 
 // Store Player Name
 function savePlayer(){
-        let player = document.getElementById('player').value;
-        sessionStorage.setItem('playerName', player);
-        document.querySelector("startBtn").removeAttribute("disabled", "");
-    }
+    let player = document.getElementById('player').value;
+    sessionStorage.setItem('playerName', player);
+    document.querySelector('#greet-player').removeAttribute('hidden', '');
     
+};
     
 let player = sessionStorage.getItem('playerName');
-    
 console.log(player);
+document.querySelector('#playerHi').innerHTML=`${player}`;
+
+    
+
+
+
+
+// document.addEventListener('DOMContentLoaded',() => {
+// newGame.start();
+
+// window.addEventListener('load', (event) => {
+//     console.log(`there was a load event`);
+// });
+
+// document.addEventListener('readystatechange', (event) => {
+//     console.log(`Ready state changed`);
+// });
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log(`DOM content loaded`);
+// });
+//////////////////////////////////////////////////////////////////////
 
 
